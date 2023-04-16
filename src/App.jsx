401bb73f1c331341a9ddfbc2e3572e2f276618f3
h0wter <howter99@gmail.com>
2023-04-16 19:59:35 +0300

    add Home and Tweets pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { lazy, Suspense, useState } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import "./App.css";
+
+const HomePage = lazy(() => import("./pages/Home"));
+const TweetsPage = lazy(() => import("./pages/Tweets"));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/tweets/" element={<TweetsPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Tweets.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+
+const TweetsPage = () => {
+  return (
+    <>
+      <h1>Tweets Page</h1>
+      <NavLink to="/">Back</NavLink>
+    </>
+  );
+};
+
+export default TweetsPage;

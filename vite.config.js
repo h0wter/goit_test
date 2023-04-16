7abb35504ad658d2839030f9f5f9bace9fdefb95
h0wter <howter99@gmail.com>
2023-04-16 16:09:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://h0wter.github.io/goit_test/",
+  base: "/goit_test/",
   plugins: [react()],
 });

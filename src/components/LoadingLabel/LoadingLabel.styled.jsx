33cc32755c9d14f25a0d0f3fbca2d8e72b730862
h0wter <howter99@gmail.com>
2023-04-18 23:27:51 +0300

    Create LoadingLabel.styled.jsx

---
+++ src/components/LoadingLabel/LoadingLabel.styled.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const LoadingLabel = styled.p`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: calc(24 / 20);
+  text-transform: uppercase;
+  margin-bottom: 16px;
+  color: #ebd8ff;
+`;

f4bc59c64c0e9bfaa4413fab453f677a7119b808
h0wter <howter99@gmail.com>
2023-04-18 22:58:03 +0300

    add Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,12 @@
-import { useState } from "react";
-import { NavLink } from "react-router-dom";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
+import { LinkButton } from "../components/LinkButton/LinkButton.styled";
+import { Container } from "./Home.styled";
 
 const HomePage = () => {
-  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <NavLink to="/tweets">Tweets</NavLink>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Container>
+      <h1>Home Page</h1>
+      <LinkButton to="/tweets">Go to Tweets</LinkButton>
+    </Container>
   );
 };
 

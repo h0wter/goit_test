ed170a0bb3f1672bbe2ddd58f5d7a25e0dc3a2ec
h0wter <howter99@gmail.com>
2023-04-18 23:27:58 +0300

    Update Tweets.styled.jsx

--- src/pages/Tweets.styled.jsx
+++ src/pages/Tweets.styled.jsx
@@ -31,14 +31,3 @@ export const LoadMoreBtn = styled.button`
     background: #5cd3a8;
   }
 `;
-
-export const LoadingLabel = styled.p`
-  font-family: "Montserrat";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 20px;
-  line-height: calc(24 / 20);
-  text-transform: uppercase;
-  margin-bottom: 16px;
-  color: #ebd8ff;
-`;

06de59109641643e095a96f968ebffb0ec85e905
h0wter <howter99@gmail.com>
2023-04-18 22:56:44 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/goit_test/",
   plugins: [react()],
+  server: {
+    errorOverlay: true,
+    clearScreen: false,
+  },
 });
